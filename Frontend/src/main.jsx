78c5ab68d71c76a60cef1eb210c90003eca12245
sayan-acharyya <acharyyas735@gmail.com>
2025-12-13 00:32:04 +0530

    createing context

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 import { BrowserRouter  } from 'react-router-dom'
+import { User } from 'lucide-react'
+import UserContext from './context/UserContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserContext>  
   </StrictMode>,
 )
